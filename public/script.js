b2a7aefd46f09ffb3632db5fc0a96c664ae37bc1
Eth <59465146+Minecrafter6432@users.noreply.github.com>
2023-12-24 19:37:50 -0800

    .bat + tts

--- public/script.js
+++ public/script.js
@@ -37,6 +37,7 @@ recognition.addEventListener("result", (e) => {
       const status = p.querySelector("#status");
       status.innerHTML = `<span class="green"></span><span>Recognized: </span>`;
       socket.emit("message", "explode");
+      speak();
     } else {
       const status = p.querySelector("#status");
       status.innerHTML = `<span class="yellow"></span><span>Unrecognized: </span>`;
@@ -65,3 +66,42 @@ clear.addEventListener("click", () => {
 });
 
 // recognition.start();
+
+function speak() {
+  const textToSpeak = "Nice Try";
+
+  // Create a new SpeechSynthesisUtterance object
+  var utterance = new SpeechSynthesisUtterance(textToSpeak);
+
+  // Get the voices available on the user's device
+  var voices = window.speechSynthesis.getVoices();
+
+  // You can set the voice using the following line if you want a specific voice
+  // utterance.voice = voices[0];
+
+  // Set other optional properties
+  utterance.rate = 1.0; // Speed of speech. 1.0 is the default.
+  utterance.pitch = 1.0; // Pitch of speech. 1.0 is the default.
+
+  // Set the voice using the first available voice
+  utterance.voice = voices[0];
+
+  var volume = parseFloat(document.getElementById("volumeSlider").value);
+  utterance.volume = volume;
+
+  // Speak the text
+  window.speechSynthesis.speak(utterance);
+}
+
+// Ensure that voices are loaded before the speak function is called
+window.speechSynthesis.onvoiceschanged = function () {
+  // Get the list of voices
+  var voices = window.speechSynthesis.getVoices();
+
+  // Print the available voices to the console (for reference)
+  console.log(voices);
+
+  // You can set the default voice here if needed
+  // var defaultVoice = voices[0];
+  // console.log(defaultVoice);
+};

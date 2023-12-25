6e5b0272cd64ddc02161ae3eab8f959feb5b1018
Eth <59465146+Minecrafter6432@users.noreply.github.com>
2023-12-24 16:56:24 -0800

    i think im done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import http from "http";
+import { WebSocketServer } from "ws";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 const server = http.createServer(app);
@@ -7,23 +9,84 @@ const server = http.createServer(app);
 import { Server } from "socket.io";
 const io = new Server(server);
 
-const port = 3000;
+const port = 5986;
+const WSport = 8954;
+
+let started = false;
 
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
-
   socket.on("message", (msg) => {
-    console.log("message: " + msg);
-
-    if (msg.includes("e")) {
-      console.log("YES IT HAS IT!");
-      // send websocket thingy to explode!
+    if (msg === "explode") {
+      if (started) {
+        sending("function explosion");
+      }
     }
   });
 });
 
 server.listen(port, () => {
-  console.log("App running on port " + port);
+  console.log(
+    "Join a Minecraft World & type /connect localhost:" + WSport + ""
+  );
+  console.log("Then open http://localhost:" + port + " in a browser & start");
 });
+
+//MINECRAFT WEBSOCKET
+const wss = new WebSocketServer({
+  port: WSport,
+});
+
+wss.on("connection", function connection(socket) {
+  console.log("WebSocket Connected W/ MINECRAFT!");
+  started = true;
+
+  socket.on("message", (packet) => {
+    const msg = JSON.parse(packet);
+    if (msg.body.message === "!test") {
+      sending("function explosion");
+    }
+  });
+
+  socket.on("close", function close() {
+    // When a client disconnects, disable mc websocket commands.
+    started = false;
+  });
+
+  socket.send(
+    JSON.stringify({
+      header: {
+        version: 1, // Use version 1 message protocol
+        requestId: uuidv4(), // A unique ID for the request
+        messageType: "commandRequest", // This is a request ...
+        messagePurpose: "subscribe", // ... to subscribe to ...
+      },
+      body: {
+        eventName: "PlayerMessage", // ... all player messages.
+      },
+    })
+  );
+});
+
+function sending(cmd) {
+  // console.log("attempting to send cmd");
+  wss.clients.forEach((client) => {
+    const msg = JSON.stringify({
+      header: {
+        version: 1,
+        requestId: uuidv4(), // Send unique ID each time
+        messagePurpose: "commandRequest",
+        messageType: "commandRequest",
+      },
+      body: {
+        version: 1,
+        commandLine: cmd, // Define the command
+        origin: {
+          type: "player", // Message comes from player
+        },
+      },
+    });
+    client.send(msg);
+  });
+}
--- public/script.js
+++ public/script.js
@@ -2,4 +2,66 @@ var socket = io();
 
 console.log("TEST");
 
-socket.emit("message", "test");
+const texts = document.querySelector(".texts");
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement("p");
+
+recognition.addEventListener("result", (e) => {
+  texts.insertBefore(p, texts.firstChild);
+  const text = Array.from(e.results)
+    .map((result) => result[0])
+    .map((result) => result.transcript)
+    .join("");
+
+  p.innerText = text;
+  const newSpan = document.createElement("span");
+  newSpan.innerHTML = `<span class="gray"></span><span>Listening: </span>`;
+  newSpan.id = "status";
+
+  p.insertBefore(newSpan, p.firstChild);
+
+  console.log(e.results);
+
+  if (e.results[0].isFinal) {
+    p.innerHTML = p.innerHTML.replace(
+      /e/g,
+      '<span style="text-decoration-line: underline; font-weight: 500;">e</span>'
+    );
+    if (text.includes("e")) {
+      const status = p.querySelector("#status");
+      status.innerHTML = `<span class="green"></span><span>Recognized: </span>`;
+      socket.emit("message", "explode");
+    } else {
+      const status = p.querySelector("#status");
+      status.innerHTML = `<span class="yellow"></span><span>Unrecognized: </span>`;
+    }
+
+    p = document.createElement("p");
+  }
+});
+
+recognition.addEventListener("end", () => {
+  recognition.start();
+});
+
+const start = document.getElementById("start");
+const titleScreen = document.getElementById("titleScreen");
+
+start.addEventListener("click", () => {
+  titleScreen.classList.add("titleScreen--hidden");
+  recognition.start();
+});
+
+const clear = document.getElementById("clear");
+
+clear.addEventListener("click", () => {
+  texts.innerHTML = "";
+});
+
+// recognition.start();
